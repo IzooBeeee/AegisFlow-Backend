544941494a0957da23487ee563d4e2e94717e7e8
IzooBeeee <vannhan130504@gmail.com>
2026-06-17 14:51:43 +0700

    feat: add FloodReportSeeder with 392 real Da Nang flood reports (Oct 2022)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // Xóa data cũ trước khi seed lại (tránh duplicate)
-        DB::statement('TRUNCATE TABLE notifications, recommendations, alerts, predictions, rescue_requests, incidents, sensors, rescue_teams, shelters, flood_zones, districts, ai_models RESTART IDENTITY CASCADE');
+        DB::statement('TRUNCATE TABLE notifications, recommendations, alerts, predictions, rescue_requests, incidents, sensors, rescue_teams, shelters, flood_zones, districts, ai_models, flood_reports RESTART IDENTITY CASCADE');
 
         $this->call([
             RolePermissionSeeder::class,
@@ -26,6 +26,7 @@ class DatabaseSeeder extends Seeder
             LocationSeeder::class,
             CitizenAndRescueTeamSeeder::class,
             EvacuationRouteSeeder::class,
+            FloodReportSeeder::class,
         ]);
     }
 }
